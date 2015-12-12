183db3355a81ba01248224226779406b4baa3c64
JoJoMimosa <joachimrochat@gmail.com>
2015-12-12 22:31:14 +0100

    d

---
+++ header.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width">
+        <title><?php wp_title( '|', true, 'right' ); ?></title>
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <!-- insert header tag here -->
